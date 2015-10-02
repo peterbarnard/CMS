a76011942e17e07473df336881f1cece7885be51
unknown <pbarnard@FST-WVLT26.FST.local>
2015-10-02 11:54:53 +0100

    Initial Setup of CRUD pages

---
+++ Competenct Management/Competenct Management/Controllers/User_Capability_Overview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Competenct_Management.Controllers
+{
+    public class User_Capability_Overview : Controller
+    {
+        // GET: User_Capability_Overview
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
